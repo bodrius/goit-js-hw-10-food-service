eb8f2a807b3ba88be03dc3150ccb5e24cf88aa76
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-02-10 23:30:40 +0200

    create menu list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import "./JS/change-theme"
 import './styles.css';
+import array from './menu';
+import menuListTemplate from './template/menu-list-temp.hbs';
+
+const refs = {
+    menuList: document.querySelector('.js-menu')
+}
+
+const markUp = menuListTemplate(array);
+refs.menuList.insertAdjacentHTML('beforeend', markUp)
+
+localStorage.setItem('theme', 'light')
