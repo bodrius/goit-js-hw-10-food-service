d4d53fcb5726c065253e4bbaa5c452087102dac7
Bogdan Pechenyk <bogdanpechenyk@gmail.com>
2020-02-11 15:45:40 +0200

    create task 10

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
-import "./JS/change-theme"
 import './styles.css';
 import array from './menu';
-import menuListTemplate from './template/menu-list-temp.hbs';
+import menuListTemplate from './template/menu-list.hbs';
 
 const refs = {
-    menuList: document.querySelector('.js-menu')
-}
+  menuList: document.querySelector('.js-menu'),
+};
 
 const markUp = menuListTemplate(array);
-refs.menuList.insertAdjacentHTML('beforeend', markUp)
+refs.menuList.insertAdjacentHTML('beforeend', markUp);
 
-localStorage.setItem('theme', 'light')
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const choiseTheme = document.querySelector('.js-switch-input');
+
+choiseTheme.addEventListener('change', savedChangeTheme);
+const themeShow = document.querySelector('body');
+
+function defaultTheme() {
+  if (localStorage.getItem('Theme') === Theme.DARK) {
+    themeShow.classList.add(Theme.DARK);
+    choiseTheme.checked = true;
+    themeShow.classList.remove(Theme.LIGHT);
+  } else {
+    localStorage.setItem('Theme', 'light-theme');
+    themeShow.classList.add(Theme.LIGHT);
+  }
+}
+defaultTheme();
+
+function savedChangeTheme(event) {
+  console.log(event);
+  if (event.target.checked === true) {
+    themeShow.classList.add(Theme.DARK);
+    themeShow.classList.remove(Theme.LIGHT);
+    localStorage.setItem('Theme', 'dark-theme');
+  } else if (event.target.checked === false) {
+    themeShow.classList.add(Theme.LIGHT);
+    themeShow.classList.remove(Theme.DARK);
+    localStorage.setItem('Theme', 'light-theme');
+  }
+}
